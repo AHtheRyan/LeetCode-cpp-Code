016a90df3e77944bc1c8506afeb6fa965018af75
Ryan Zhang <46887748+AHtheRyan@users.noreply.github.com>
2021-09-03 17:06:17 +0800

    Update #面试题17.14 Smallest K LCCI 最小K个数.cpp

--- #面试题17.14 Smallest K LCCI 最小K个数.cpp
+++ #面试题17.14 Smallest K LCCI 最小K个数.cpp
@@ -1,9 +1,9 @@
 /*
-    ��Ϊ��ǰ k ��Ԫ�أ����ֱ�׵ķ�ʽ�����������ǰ k ����
-    ���� C++ �� STL ֱ�ӱ�̼���
+    因为求前 k 个元素，最简单直白的方式就是排序后求前 k 个，
+    利用 C++ 的 STL 直接编程即可
 */ 
 
-I ����
+I 排序
 vector<int> smallestK(vector<int>& arr, int k)
 {
     sort(arr.begin(), arr.end());
@@ -12,12 +12,12 @@ vector<int> smallestK(vector<int>& arr, int k)
 }
 
 /*
-    ���������ʱ�临�Ӷ�Ϊ o(nlogn)��
-    ����ԭ���飬ͬʱά��һ�������飬ʹ��һֱ�洢ǰ k ������֣������� o(n)ʱ���ڽ�����⣬
-    �������ȶ��п���ʡȥ���������ڴ����ͱȽϵ��鷳
+    利用排序的时间复杂度为 o(nlogn)，
+    遍历原数组，同时维护一个新数组，使其一直存储前 k 大的数字，即可在 o(nlogk)时间内解决问题，
+    采用优先队列可以省去在新数组内处理和比较的麻烦
 */
 
-II ���ȶ���
+II 优先队列
 vector<int> smallestK(vector<int>& arr, int k)
 {
     if (k == 0)
